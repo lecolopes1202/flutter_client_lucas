6104912b18ff7526df8a81a65cf6113b10e68d2f
lecolopes1202 <alexlopes1202@gmail.com>
2021-08-18 11:56:46 -0300

    employeeModel e falta Register

--- lib/Model/EmployeeModel.dart
+++ lib/Model/EmployeeModel.dart
@@ -9,23 +9,29 @@ class EmployeeModel {
   int id;
   String firstName;
   String lastName;
-  //String mapLocXY; /mapa
+  String mapLocX; // mapa
+  String mapLocY; // mapa
+  
 
-  EmployeeModel({this.id, this.firstName, this.lastName}); // this.mapLocXY
+  EmployeeModel({this.id, this.firstName, this.lastName, this.mapLocX,this.mapLocY}); // this.mapLocXY
 
   factory EmployeeModel.fromJson(Map<String, dynamic> json) => EmployeeModel(
-      firstName: json["firstName"], lastName: json["lastName"], id: json["id"]); //mapcoordenada :json["mapLocXY"]
+      firstName: json["firstName"], lastName: json["lastName"], id: json["id"],
+      mapLocX: json["mapLocX"],mapLocY: json["mapLocY"] ); //mapLocXY :json["mapLocXY"]
 
   Map<String, dynamic> toJson() => {
         "firstName": firstName,
         "lastName": lastName,
         'id': id,
-        //'mapLocXY': mapLocXY,
+        'mapLocX': mapLocX, //'mapLocXY': mapLocXY,
+        'mapLocY': mapLocY, //'mapLocXY': mapLocXY,
       };
 
   String get firstname => firstName;
 
   String get lastname => lastName;
 
-  //String ? get maplocxy => mapLocXY;
+  String get maplocx => mapLocX; //String ? get maplocxy => mapLocXY;
+
+  String get maplocy => mapLocY;
 }
